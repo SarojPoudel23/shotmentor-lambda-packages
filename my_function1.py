1c63bb4ac1e661bce3840421ac5ca7484db602dd
Saroj Poudel <saroj.poudel@outcodesoftware.com>
2024-07-02 11:09:36 +0545

    lambda packages for ecr deployment

--- my_function1.py
+++ my_function1.py
@@ -1,18 +1,28 @@
 import json
-def lambda_handler(event, context):
+import logging
+import cv2
+import pandas as pd
+import mediapipe as mp
+import numpy as np
+
+def lambda_handler1(event, context):
     try:
         # Get the HTTP body from the event
         http_body = event['body']
+        logging.info(f"Received HTTP body: {http_body}")
 
         # Parse the JSON data
         data = json.loads(http_body)
+        logging.info(f"Parsed JSON data: {data}")
 
         # Access the 'first_name' property from the JSON
         first_name = data.get('first_name', 'Unknown')
 
+        versions = f'OpenCV: {cv2.__version__}, Pandas: {pd.__version__}, Mediapipe: {mp.__version__}, Numpy: {np.__version__}'
+
         return {
             'statusCode': 200,
-            'body': json.dumps({'message': f'Hello, {first_name}!'})
+            'body': json.dumps({'message': f'Hello, {first_name}!', 'versions': versions})
         }
     except KeyError as e:
         logging.error(f"KeyError: {str(e)}")
@@ -29,6 +39,6 @@ def lambda_handler(event, context):
     except Exception as e:
         logging.error(f"Unexpected error: {str(e)}")
         return {
-            'statusCode': 500,
-            'body': json.dumps({'error': 'Internal server error'})
+            'statusCode': 200,
+            'body': json.dumps({'error': 'There is an Internal server error'})
         }
